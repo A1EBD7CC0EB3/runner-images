4c3c3b1e4f755ddc489dae8ac99a0ddb48e32ffb
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2022-09-01 16:52:53 -0400

    Update create-base-image.sh

--- images/linux/create-base-image.sh
+++ images/linux/create-base-image.sh
@@ -2,8 +2,8 @@
 
 git clone https://github.com/actions-runner-controller/actions-runner-controller.git
 cd actions-runner-controller/runner
-sed -i 's/ubuntu:20.04/ubuntu:22.04/g' action-runner.dockerfile
-docker build -t runner-base:22.04 -f action-runner.dockerfile .
+sed -i 's/ubuntu:20.04/ubuntu:22.04/g' actions-runner.dockerfile
+docker build -t runner-base:22.04 -f actions-runner.dockerfile .
 
 cd ../../
 rm -rf actions-runner-controller
