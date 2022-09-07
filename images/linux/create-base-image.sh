83aff26a12f68d985a93e883a857ff5f5bea8e67
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2022-09-07 09:46:34 -0400

    Add required arg

--- images/linux/create-base-image.sh
+++ images/linux/create-base-image.sh
@@ -3,7 +3,11 @@
 git clone https://github.com/actions-runner-controller/actions-runner-controller.git
 cd actions-runner-controller/runner
 sed -i 's/ubuntu:20.04/ubuntu:22.04/g' actions-runner-dind.dockerfile
-docker build -t runner-base:22.04 -f actions-runner-dind.dockerfile .
+docker build \
+    --build-arg "TARGET_PLATFORM=linux/amd64"
+    -t runner-base:22.04 \
+    -f actions-runner-dind.dockerfile \
+    .
 
 cd ../../
 rm -rf actions-runner-controller
